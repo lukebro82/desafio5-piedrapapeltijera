b6e32040111215f2860e5945dfae5ea6008dad37
Lucas <lucas.82@hotmail.com.ar>
2024-03-05 19:21:45 -0300

    Updates

--- index.5b148a20.js
+++ index.5b148a20.js
@@ -1,4 +1,4 @@
-var e=globalThis,t={},a={},n=e.parcelRequire17cc;null==n&&((n=function(e){if(e in t)return t[e].exports;if(e in a){var n=a[e];delete a[e];var r={id:e,exports:{}};return t[e]=r,n.call(r.exports,r,r.exports),r.exports}var s=Error("Cannot find module '"+e+"'");throw s.code="MODULE_NOT_FOUND",s}).register=function(e,t){a[e]=t},e.parcelRequire17cc=n);var r=n.register;r("27Lyk",function(e,t){Object.defineProperty(e.exports,"register",{get:()=>a,set:e=>a=e,enumerable:!0,configurable:!0});var a,n=new Map;a=function(e,t){for(var a=0;a<t.length-1;a+=2)n.set(t[a],{baseUrl:e,path:t[a+1]})}}),r("6K86f",function(e,t){e.exports=new URL("tijera.6a2fdda1.svg",import.meta.url).toString()}),r("c2Nan",function(e,t){e.exports=new URL("piedra.c88f7ccf.svg",import.meta.url).toString()}),r("dmhka",function(e,t){e.exports=new URL("papel.5f79bbf0.svg",import.meta.url).toString()}),r("Sd0uU",function(e,t){e.exports=new URL("ganaste.1eea6986.svg",import.meta.url).toString()}),r("ieP5C",function(e,t){e.exports=new URL("empate.acc190a0.png",import.meta.url).toString()}),r("cGuk0",function(e,t){e.exports=new URL("perdiste.342a139d.svg",import.meta.url).toString()}),n("27Lyk").register(new URL("",import.meta.url).toString(),JSON.parse('["1LzKV","index.44be4874.js","cRr7T","tijera.6a2fdda1.svg","luIbU","piedra.c88f7ccf.svg","a8QNb","papel.5f79bbf0.svg","edDk0","ganaste.1eea6986.svg","2W9uc","empate.acc190a0.png","dlhiO","perdiste.342a139d.svg"]'));const s={data:{currentGame:{computerPlay:"",userPlay:""},history:{computerScore:0,userScore:0}},listeners:[],setUserMove(e){this.getState().currentGame.userPlay=e},setComputerMove(e){this.getState().currentGame.computerPlay=e},runPlayComputer(){let e=Math.floor(9*Math.random())+1,t="papel";t=e<4?"piedra":e>=4&&e<=6?"papel":"tijera",this.setComputerMove(t)},whoWins(e,t){let a="empate";return"papel"==e&&"piedra"==t&&(a="user"),"papel"==e&&"tijera"==t&&(a="computer"),"piedra"==e&&"tijera"==t&&(a="user"),"piedra"==e&&"papel"==t&&(a="computer"),"tijera"==e&&"papel"==t&&(a="user"),"tijera"==e&&"piedra"==t&&(a="computer"),this.pushHistory(a),a},pushHistory(e){let t=this.getState();"computer"==e?t.history.computerScore++:"user"==e&&t.history.userScore++},getState(){return this.data},setState(e){for(let t of(this.data=e,this.listeners))t()},subscribe(e){this.listeners.push(e)}},o=[{path:/\/welcome/,component:function(e){let t=document.createElement("div");t.className="welcome",t.innerHTML=`
+var e=globalThis,t={},a={},n=e.parcelRequire17cc;null==n&&((n=function(e){if(e in t)return t[e].exports;if(e in a){var n=a[e];delete a[e];var r={id:e,exports:{}};return t[e]=r,n.call(r.exports,r,r.exports),r.exports}var s=Error("Cannot find module '"+e+"'");throw s.code="MODULE_NOT_FOUND",s}).register=function(e,t){a[e]=t},e.parcelRequire17cc=n);var r=n.register;r("27Lyk",function(e,t){Object.defineProperty(e.exports,"register",{get:()=>a,set:e=>a=e,enumerable:!0,configurable:!0});var a,n=new Map;a=function(e,t){for(var a=0;a<t.length-1;a+=2)n.set(t[a],{baseUrl:e,path:t[a+1]})}}),r("b7CwH",function(e,t){e.exports=new URL("tijera.6a2fdda1.svg",import.meta.url).toString()}),r("6O4FV",function(e,t){e.exports=new URL("piedra.c88f7ccf.svg",import.meta.url).toString()}),r("gYWi2",function(e,t){e.exports=new URL("papel.5f79bbf0.svg",import.meta.url).toString()}),r("eluKL",function(e,t){e.exports=new URL("ganaste.1eea6986.svg",import.meta.url).toString()}),r("b70r7",function(e,t){e.exports=new URL("empate.acc190a0.png",import.meta.url).toString()}),r("jqGhO",function(e,t){e.exports=new URL("perdiste.342a139d.svg",import.meta.url).toString()}),n("27Lyk").register(new URL("",import.meta.url).toString(),JSON.parse('["dZpbI","index.5b148a20.js","cK2FM","tijera.6a2fdda1.svg","lyYKJ","piedra.c88f7ccf.svg","jYzGd","papel.5f79bbf0.svg","c1fAi","ganaste.1eea6986.svg","2w0Vc","empate.acc190a0.png","eA72X","perdiste.342a139d.svg"]'));const s={data:{currentGame:{computerPlay:"",userPlay:""},history:{computerScore:0,userScore:0}},listeners:[],setUserMove(e){this.getState().currentGame.userPlay=e},setComputerMove(e){this.getState().currentGame.computerPlay=e},runPlayComputer(){let e=Math.floor(9*Math.random())+1,t="papel";t=e<4?"piedra":e>=4&&e<=6?"papel":"tijera",this.setComputerMove(t)},whoWins(e,t){let a="empate";return"papel"==e&&"piedra"==t&&(a="user"),"papel"==e&&"tijera"==t&&(a="computer"),"piedra"==e&&"tijera"==t&&(a="user"),"piedra"==e&&"papel"==t&&(a="computer"),"tijera"==e&&"papel"==t&&(a="user"),"tijera"==e&&"piedra"==t&&(a="computer"),this.pushHistory(a),a},pushHistory(e){let t=this.getState();"computer"==e?t.history.computerScore++:"user"==e&&t.history.userScore++},getState(){return this.data},setState(e){for(let t of(this.data=e,this.listeners))t()},subscribe(e){this.listeners.push(e)}},o=[{path:/\/welcome/,component:function(e){let t=document.createElement("div");t.className="welcome",t.innerHTML=`
   <h1 class="welcome-title">Piedra</h1>
   <h1 class="welcome-title">Papel</h1>
   <h1 class="welcome-title">Tijera</h1>
@@ -33,7 +33,7 @@ var e=globalThis,t={},a={},n=e.parcelRequire17cc;null==n&&((n=function(e){if(e i
                   <hands-el hand="tijera" type="${o}" class="tijera"></hands-el>
                  </div>
 
-                 `;let d=t.querySelector(".piedra"),l=t.querySelector(".papel"),c=t.querySelector(".tijera");d?.addEventListener("click",()=>{n="hand-grande",r="hand-none",o="hand-none",s.setUserMove("piedra")}),l?.addEventListener("click",()=>{n="hand-none",r="hand-grande",o="hand-none",s.setUserMove("papel")}),c?.addEventListener("click",()=>{n="hand-none",r="hand-none",o="hand-grande",s.setUserMove("tijera")}),0===a&&(clearInterval(i),s.runPlayComputer(),e.goTo("/versus"))},1e3);return t}},{path:/\/versus/,component:function(e){let t=document.createElement("div");t.className="select-end";let a=s.getState(),n=a.currentGame.computerPlay,r=a.currentGame.userPlay;t.innerHTML=`  
+                 `;let l=t.querySelector(".piedra"),d=t.querySelector(".papel"),c=t.querySelector(".tijera");l?.addEventListener("click",()=>{n="hand-grande",r="hand-none",o="hand-none",s.setUserMove("piedra")}),d?.addEventListener("click",()=>{n="hand-none",r="hand-grande",o="hand-none",s.setUserMove("papel")}),c?.addEventListener("click",()=>{n="hand-none",r="hand-none",o="hand-grande",s.setUserMove("tijera")}),0===a&&(clearInterval(i),s.runPlayComputer(),e.goTo("/versus"))},1e3);return t}},{path:/\/versus/,component:function(e){let t=document.createElement("div");t.className="select-end";let a=s.getState(),n=a.currentGame.computerPlay,r=a.currentGame.userPlay;t.innerHTML=`  
   
   <hands-el hand="${n}" type="hand-grande" class="select-end-computer"></hands-el>
   <hands-el hand="${r}"  type="hand-grande" class="select-end-user"></hands-el>
@@ -48,7 +48,7 @@ var e=globalThis,t={},a={},n=e.parcelRequire17cc;null==n&&((n=function(e){if(e i
     
     <el-button class="button-reinicar">Volver</el-button></div>
   
-        `;let d=i.querySelector(".button-reinicar");return d?.addEventListener("click",()=>{e.goTo("/welcome")}),i}}],i={tijera:n("6K86f"),piedra:n("c2Nan"),papel:n("dmhka")};class d extends HTMLElement{constructor(){super(),this.shadow=this.attachShadow({mode:"open"});let e=document.createElement("style");e.textContent=`
+        `;let l=i.querySelector(".button-reinicar");return l?.addEventListener("click",()=>{e.goTo("/welcome")}),i}}],i={tijera:n("b7CwH"),piedra:n("6O4FV"),papel:n("gYWi2")};class l extends HTMLElement{constructor(){super(),this.shadow=this.attachShadow({mode:"open"});let e=document.createElement("style");e.textContent=`
           .hand-img {           
               width: 125px;
               height: 225px;   
@@ -80,7 +80,7 @@ var e=globalThis,t={},a={},n=e.parcelRequire17cc;null==n&&((n=function(e){if(e i
           }
           `,this.render(),this.shadow.appendChild(e)}render(){let e=this.getAttribute("type"),t=this.getAttribute("hand");this.shadow.innerHTML=`
           <img class="${e}" src="${i[t]}">
-          `}}customElements.define("hands-el",d);class l extends HTMLElement{constructor(){super(),this.shadow=this.attachShadow({mode:"open"});let e=document.createElement("style");e.innerHTML=`
+          `}}customElements.define("hands-el",l);class d extends HTMLElement{constructor(){super(),this.shadow=this.attachShadow({mode:"open"});let e=document.createElement("style");e.innerHTML=`
     .root{
           width: 330px;
           height: 80px;
@@ -92,7 +92,7 @@ var e=globalThis,t={},a={},n=e.parcelRequire17cc;null==n&&((n=function(e){if(e i
           font-size: 45px;
           font-family: "Work Sans", sans-serif;
           color: white;
-         }`,this.shadow.appendChild(e),this.render()}render(){let e=document.createElement("button");e.textContent=this.textContent,e.className="root",this.shadow.appendChild(e)}}customElements.define("el-button",l);class c extends HTMLElement{constructor(){super(),this.shadow=this.attachShadow({mode:"open"});let e=document.createElement("style");e.innerHTML=`
+         }`,this.shadow.appendChild(e),this.render()}render(){let e=document.createElement("button");e.textContent=this.textContent,e.className="root",this.shadow.appendChild(e)}}customElements.define("el-button",d);class c extends HTMLElement{constructor(){super(),this.shadow=this.attachShadow({mode:"open"});let e=document.createElement("style");e.innerHTML=`
       .score {
         font-family: "Work Sans", sans-serif;
         width: 250px;
@@ -129,7 +129,7 @@ var e=globalThis,t={},a={},n=e.parcelRequire17cc;null==n&&((n=function(e){if(e i
           <h1 class="textoh2">Compu: ${a}</h1>
     </div>
    
-       `,this.shadow.appendChild(n)}}customElements.define("score-el",c);const h={Ganaste:n("Sd0uU"),Empate:n("ieP5C"),Perdiste:n("cGuk0")};class p extends HTMLElement{constructor(){super(),this.shadow=this.attachShadow({mode:"open"});let e=document.createElement("style");e.innerHTML=`
+       `,this.shadow.appendChild(n)}}customElements.define("score-el",c);const h={Ganaste:n("eluKL"),Empate:n("b70r7"),Perdiste:n("jqGhO")};class p extends HTMLElement{constructor(){super(),this.shadow=this.attachShadow({mode:"open"});let e=document.createElement("style");e.innerHTML=`
       
     .estrella {
         display: flex;
@@ -161,5 +161,5 @@ var e=globalThis,t={},a={},n=e.parcelRequire17cc;null==n&&((n=function(e){if(e i
     <img class="imagen" src="${h[e]}">
     <h1 class="texto">${e}</h1>
   </div>
-          `}}customElements.define("winlost-el",p),function(e){function t(e){history.pushState({},"",e),a(e)}function a(a){if(location.host.includes("github.io")){let n=a.replace("desafio5-piedrapapeltijera","/");for(let a of o)if(a.path.test(n)){let n=a.component({goTo:t});e.firstChild&&e.firstChild.remove(),e.appendChild(n)}}else for(let n of o)if(n.path.test(a)){let a=n.component({goTo:t});e.firstChild&&e.firstChild.remove(),e.appendChild(a)}}"/"==location.pathname?t("/welcome"):a(location.pathname)}(document.querySelector(".root"));
-//# sourceMappingURL=index.44be4874.js.map
+          `}}customElements.define("winlost-el",p),function(e){function t(e){history.pushState({},"",e),a(e)}function a(a){if(location.host.includes("github.io")){let n=a.replace("/desafio5-piedrapapeltijera","/");for(let a of o)if(a.path.test(n)){let n=a.component({goTo:t});e.firstChild&&e.firstChild.remove(),e.appendChild(n)}}else for(let n of o)if(n.path.test(a)){let a=n.component({goTo:t});e.firstChild&&e.firstChild.remove(),e.appendChild(a)}}"/"==location.pathname?t("/welcome"):a(location.pathname)}(document.querySelector(".root"));
+//# sourceMappingURL=index.5b148a20.js.map
